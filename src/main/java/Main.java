78ecdb228a2c8f9ff4403690c8d4a9e8fe7be847
Saikumar <skaparthi2@outlook.com>
2024-05-01 09:59:34 +0530

    f1

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+        ServletContextHandler handler = new ServletContextHandler();
+        handler.addServlet(new ServletHolder(new HelloServlet()), "/hello");
+        server.setHandler(handler);
+        server.start();
+        server.join();
+    }
+}
